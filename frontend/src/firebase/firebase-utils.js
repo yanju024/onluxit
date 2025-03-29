5469c9f5546496d7a4136a4ec87c2c975edbe62b
yanju024 <yanju024@gmail.com>
2025-03-29 05:30:43 +0530

    Update firebase-utils.js
    updated firebase auth

--- frontend/src/firebase/firebase-utils.js
+++ frontend/src/firebase/firebase-utils.js
@@ -3,15 +3,7 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 
-const config = {
-    apiKey: "AIzaSyDy716QStsMe_SsqzB1fHQrdrUgoiDrrR0",
-    authDomain: "crwn-clothing-3656c.firebaseapp.com",
-    projectId: "crwn-clothing-3656c",
-    storageBucket: "crwn-clothing-3656c.firebasestorage.app",
-    messagingSenderId: "617030052845",
-    appId: "1:617030052845:web:ea9fd6021afb0e0cdb55e4",
-    measurementId: "G-27S496J3Y7"
-}
+
 
 const firebaseApp = firebase.initializeApp(config);
 
