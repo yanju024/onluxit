5cba50c5e2266c96b2adeb28c99d9333e12b395f
yanju024 <yanju024@gmail.com>
2025-04-01 20:15:04 +0530

    Update firebase-utils.js

--- frontend/src/firebase/firebase-utils.js
+++ frontend/src/firebase/firebase-utils.js
@@ -3,13 +3,13 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
 const config = {
-  apiKey: "AIzaSyDy716QStsMe_SsqzB1fHQrdrUgoiDrrR0",
-  authDomain: "OnLuxit-clothing-3656c.firebaseapp.com",
-  projectId: "OnLuxit-clothing-3656c",
-  storageBucket: "OnLuxit-clothing-3656c.firebasestorage.app",
-  messagingSenderId: "617030052845",
-  appId: "1:617030052845:web:ea9fd6021afb0e0cdb55e4",
-  measurementId: "G-27S496J3Y7",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 const firebaseApp = firebase.initializeApp(config);
